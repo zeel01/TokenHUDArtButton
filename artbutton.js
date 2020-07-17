226ebbafc6a0db2d196c88a9cf504377e404d233
zeel <zeel02@gmail.com>
2020-07-17 12:21:09 -0400

    Filled out module.json and cleaned up code.

--- artbutton.js
+++ artbutton.js
@@ -1,12 +1,13 @@
 Hooks.on("renderTokenHUD", (hud, html, token) => {
-	console.debug(hud, html, token);
 	let actor = game.actors.get(token.actorId);
 	
 	let artButton = document.createElement("div");
+	
 	artButton.classList.add("control-icon");
 	artButton.classList.add("artwork-open");
 	artButton.innerHTML = `<i class="fas fa-image fa-fw"></i>`
 	artButton.title = "Click to view artwork. Right-click to view token image."
+	
 	$(artButton).click((event) => {
 		new ImagePopout(actor.data.img, {
 			title: actor.name,
@@ -20,18 +21,6 @@ Hooks.on("renderTokenHUD", (hud, html, token) => {
 			shareable: true,
 		}).render(true);
 	});
-	
-//	let tokenButton = document.createElement("div");
-//	tokenButton.classList.add("control-icon");
-//	tokenButton.classList.add("artwork-open");
-//	tokenButton.innerHTML = `<i class="fas fa-user-circle"></i>`
-//	$(tokenButton).click((event) => {
-//		new ImagePopout(token.img, {
-//			title: token.name,
-//			shareable: true,
-//		}).render(true);
-//	});
-	
+
 	html.find("div.left").append(artButton);
-//	html.find("div.right").append(tokenButton);
 });
