4e30739966da8690765c611df2836f67bf3ae0f2
zeel <zeel02@gmail.com>
2020-08-06 00:12:38 -0400

    Updated the way actor images are handled. Won't show the mystery man if not nessesary. Won't display the name of an actor if that name isn't visible to everyone.

--- artbutton.js
+++ artbutton.js
@@ -1,23 +1,39 @@
 Hooks.on("renderTokenHUD", (hud, html, token) => {
-	let actor = game.actors.get(token.actorId);
-	
+	let actor = game.actors.get(token.actorId); 
+	let synthActor = token.actorData;
+
 	let artButton = document.createElement("div");
 	
 	artButton.classList.add("control-icon");
 	artButton.classList.add("artwork-open");
 	artButton.innerHTML = `<i class="fas fa-image fa-fw"></i>`
-	artButton.title = "Click to view artwork. Right-click to view token image."
+	artButton.title = game.i18n.localize("TKNHAB.TooltipText");
 	
+	let showName = 	token.displayname == CONST.TOKEN_DISPLAY_MODES.ALWAYS ||
+					token.displayname == CONST.TOKEN_DISPLAY_MODES.HOVER
+	let hiddenName = game.i18n.localize("DND5E.Name");
+
+	let actorImg = synthActor.img || actor.data.img;
+	let tokenImg = token.img;
+
+	let am = actorImg === "icons/svg/mystery-man.svg";
+	let tm = tokenImg === "icons/svg/mystery-man.svg";
+	
+	if (!(am && tm)) {
+		actorImg = am ? tokenImg : actorImg;
+		tokenImg = tm ? actorImg : tokenImg;
+	}
+
 	$(artButton).click((event) => {
-		new ImagePopout(actor.data.img, {
-			title: actor.name,
+		new ImagePopout(actorImg , {
+			title: showName ? synthActor.name || actor.name : hiddenName,
 			shareable: true,
 			uuid: actor.uuid
 		}).render(true);
 	});
 	$(artButton).contextmenu((event) => {
-		new ImagePopout(token.img, {
-			title: token.name,
+		new ImagePopout(tokenImg, {
+			title: showName ? token.name : hiddenName,
 			shareable: true,
 		}).render(true);
 	});
