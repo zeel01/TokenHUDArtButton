8759ee462b417288d56d16ca833582b727e7c1e9
zeel <zeel02@gmail.com>
2020-09-02 00:39:47 -0400

    Added JSDoc comments.

--- artbutton.js
+++ artbutton.js
@@ -1,7 +1,75 @@
+/**
+ * This class is used as a namespace for Show Art
+ * static methods. It has no constructor.
+ *
+ * @class ShowArt
+ */
 class ShowArt {
+	/**
+	 * Handles the keydown events for tile and token keybindings
+	 *
+	 * @static
+	 * @param {Event} event - The triggering event.
+	 * @param {string} image - The file path of the image to display.
+	 * @param {string} title - The name to display in the popup title bar.
+	 * @memberof ShowArt
+	 */
+	static keyEventHandler(event, image, title) {
+		if (event.shiftKey && (event.key == "Z" || event.key == "X")) {
+			const pop = this.createImagePopup(image, title);
+			if (!event.altKey && game.user.isGM) pop.shareImage();
+		}
+	}
+	/**
+	 * Handles the click or contextmenu events for tile/token art buttons
+	 *
+	 * @static
+	 * @param {Event} event - The triggering event.
+	 * @param {string} image - The file path of the image to display.
+	 * @param {string} title - The name to display in the popup title bar.
+	 * @memberof ShowArt
+	 */
+	static buttonEventHandler(event, image, title) {
+		const pop = this.createImagePopup(image, title);
+		if (event.shiftKey && game.user.isGM) pop.shareImage();
+	}
+	/**
+	 * Creates and renders and ImagePopout
+	 * with a specific image and title.
+	 * the image is set to sharable.
+	 *
+	 * @static
+	 * @param {string} image - The file path of the image to display.
+	 * @param {string} title - The name to display in the popup title bar.
+	 * @return {ImagePopout} The instance of the ImagePopout.
+	 * @memberof ShowArt
+	 */
+	static createImagePopup(image, title) {
+		return new ImagePopout(image, {
+			title, shareable: true,
+		}).render(true);
+	}
+	/**
+	 * Retrieves the Actor associated with a given token.
+	 *
+	 * @static
+	 * @param {Token} token - The Token to look for the Actor of.
+	 * @return {Actor} The associated Actor.
+	 * @memberof ShowArt
+	 */
 	static getTokenActor(token) {
 		return game.actors.get(token.actorId); 
 	}
+	/**
+	 * Determin the correct image titles for either the token,
+	 * or the associated Actor.
+	 *
+	 * @static
+	 * @param {Token} token - The Token to get the title of.
+	 * @param {Actor} actor - The Actor to get the title of.
+	 * @return {object} { actor, token } The titles for actor and token.
+	 * @memberof ShowArt
+	 */
 	static getTokenTitles(token, actor) {
 		const	M = CONST.TOKEN_DISPLAY_MODES,
 				dn = token.displayname;
@@ -14,6 +82,16 @@ class ShowArt {
 		const hiddenName = game.i18n.localize("DND5E.Name");
 		return { actor: hiddenName, token: hiddenName }
 	}
+	/**
+ 	 * Determin the correct image paths for either the token,
+	 * or the associated Actor.
+	 *
+	 * @static
+	 * @param {Token} token - The Token to get the path of.
+	 * @param {Actor} actor - The Actor to get the path of.
+	 * @return {object} { actor, token } The paths of the actor and token images. 
+	 * @memberof ShowArt
+	 */
 	static getTokenImages(token, actor) {
 		const mystery = "icons/svg/mystery-man.svg";
 		const synthActor = token.actorData;
@@ -31,6 +109,14 @@ class ShowArt {
 
 		return { actor: actorImg, token: tokenImg };
 	}
+	/**
+	 * Create the HTML elements for the HUD button
+	 * including the Font Awesome icon and tooltop.
+	 *
+	 * @static
+	 * @return {Element} The `<div>` element that is used as the HUD button.
+	 * @memberof ShowArt
+	 */
 	static createButton() {
 		let button = document.createElement("div");
 
@@ -41,6 +127,16 @@ class ShowArt {
 
 		return button;
 	}
+
+	/**
+	 * Adds the keybinding to the selected tile.
+	 *
+	 * @static
+	 * @param {Tile} tile - The selected Tile.
+	 * @param {Boolean} control - Whether or not this Tile is being selected, or deselected.
+	 * @return {null} Early return if control is false.
+	 * @memberof ShowArt
+	 */
 	static prepTileKeybinding(tile, control) {
 		const doc = $(document);
 		doc.off("keydown.showArt");
@@ -50,6 +146,15 @@ class ShowArt {
 			this.keyEventHandler(event, tile.data.img, game.i18n.localize("TKNHAB.TileImage"))
 		);
 	}
+	/**
+	 * Adds the keybinding to the selected token.
+	 *
+	 * @static
+	 * @param {Token} token - The selected Token.
+	 * @param {Boolean} control - Whether or not this Token is being selected, or deselected.
+	 * @return {null} Early return if control is false.
+	 * @memberof ShowArt
+	 */
 	static prepTokenKeybinding(token, control) {
 		const doc = $(document);
 		doc.off("keydown.showArt");
@@ -67,6 +172,16 @@ class ShowArt {
 			)
 		);
 	}
+	/**
+	 * Adds the button to the Token HUD,
+	 * and attaches event listeners.
+	 *
+	 * @static
+	 * @param {TokenHUD} hud - The HUD object, not used.
+	 * @param {jQuery} html - The jQuery reference to the HUD HTML.
+	 * @param {Token} token - The data for the Token.
+	 * @memberof ShowArt
+	 */
 	static prepTokenHUD(hud, html, token) {
 		const actor = this.getTokenActor(token);
 		const images = this.getTokenImages(token, actor);
@@ -83,6 +198,16 @@ class ShowArt {
 
 		html.find("div.left").append(artButton);
 	}
+	/**
+	 * Adds the button to the Tile HUD,
+	 * and attaches event listeners.
+	 *
+	 * @static
+	 * @param {TileHUD} hud - The HUD object, not used.
+	 * @param {jQuery} html - The jQuery reference to the HUD HTML.
+	 * @param {Tile} token - The data for the Tile.
+	 * @memberof ShowArt
+	 */
 	static prepTileHUD(hud, html, tile) {
 		const artButton = this.createButton();
 
@@ -94,21 +219,6 @@ class ShowArt {
 				)
 			)
 		html.find("div.left").append(artButton);
-	}
-	static keyEventHandler(event, image, title) {
-		if (event.shiftKey && (event.key == "Z" || event.key == "X")) {
-			const pop = this.createImagePopup(image, title);
-			if (!event.altKey && game.user.isGM) pop.shareImage();
-		}
-	}
-	static buttonEventHandler(even, image, title) {
-		const pop = this.createImagePopup(image, title);
-		if (event.shiftKey && game.user.isGM) pop.shareImage();
-	}
-	static createImagePopup(image, title) {
-		return new ImagePopout(image, {
-			title, shareable: true,
-		}).render(true);
 	}
 }
 
